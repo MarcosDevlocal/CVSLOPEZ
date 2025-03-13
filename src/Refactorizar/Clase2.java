8cbf96a3dff13c3fefa05bb657b9bfbad254a940
twden <twden@ENVY14>
2025-03-13 13:42:40 +0100

    Segundo Commit

---
+++ src/Refactorizar/Clase2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Refactorizar;
+
+/**
+ *
+ * @author twden
+ */
+public class Clase2 {
+    public static void main(String[] args) {
+        String rama = "PRODUCCION";
+        System.out.println("Estas en la rama:  " + rama);
+    }
+  
+}

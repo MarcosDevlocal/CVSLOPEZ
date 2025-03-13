398dbf888b1720d9ae83b29e1368b2d1fb985011
twden <twden@ENVY14>
2025-03-13 13:36:43 +0100

    Primer Commit

---
+++ src/Refactorizar/Clase1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Refactorizar;
+
+/**
+ *
+ * @author twden
+ */
+public class Clase1 {
+  String nombre = "Marcos";
+  String apellido = "Lopez";
+    
+}

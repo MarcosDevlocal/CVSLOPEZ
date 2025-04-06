10ad66be38f50d2f457d4d684115b37b1e07c5cc
twden <twden@192.168.1.132>
2025-04-06 16:03:45 +0200

    Segundo Commit

---
+++ src/c/Clase2.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package c;
+
+/**
+ *
+ * @author twden
+ */
+public class Clase2 {
+    String saludo = "Estas en la rama de produccion";
+    int codigo = 8217918;
+}

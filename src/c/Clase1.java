83c7b6b8a482fea3a03dc987d032b561dae567bf
twden <twden@192.168.1.132>
2025-04-06 15:48:35 +0200

    Primer Commit

---
+++ src/c/Clase1.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package c;
+
+/**
+ *
+ * @author twden
+ */
+public class Clase1 {
+    String nombre = "Marcos";
+    String apellidos = "Lopez Ortego";
+}
